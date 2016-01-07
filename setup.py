f026fa30c9f65bf9bca23cc968e4f5f454754892
Tom Hayden <thayden@gmail.com>
2016-01-06 23:41:41 -0600

    upgrading to 0.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='bada',
-    version='0.0.1',
+    version='0.0.2',
     description='Work seamlessly with AWS Lambda Jobs',
     url='https://github.com/haydenth/bada',
     license='MIT',
