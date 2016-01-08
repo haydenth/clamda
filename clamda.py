90f978005f6bd4667345f02223683555f4a30674
Tom Hayden <thayden@gmail.com>
2016-01-07 23:27:57 -0600

    smiling clam

--- clamda.py
+++ clamda.py
@@ -4,8 +4,8 @@ for working with aws lambda and making
 the dev workflow better 
 
        _.---._
-   .:":(o)'(o)":.
-  :` _'_-.-._'_.':
+   .:":(O)'(O)":.
+  :` _'\_____/_.':
   '`.`._`-.-'_.'.''
    `.`-.`-.-'.-'.'
     `._`-.-'_.'
