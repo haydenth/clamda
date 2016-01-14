cb10e558994d765732a5c131992470722c193d6d
Tom Hayden <thayden@gmail.com>
2016-01-13 22:16:24 -0600

    updating to 0.0.7

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='clamda',
-    version='0.0.6',
+    version='0.0.7',
     description='Work seamlessly with AWS Lambda Jobs',
     url='https://github.com/haydenth/clamda',
     license='MIT',
