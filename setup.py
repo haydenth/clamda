ea04274e995620306797b6891da555e854a3ac66
Tom Hayden <thayden@gmail.com>
2017-10-26 11:38:41 -0500

    +1 version id

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='clamda',
-    version='0.0.10',
+    version='0.0.11',
     description='Work seamlessly with AWS Lambda Jobs',
     url='https://github.com/haydenth/clamda',
     license='MIT',
